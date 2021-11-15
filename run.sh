aadf22917e8ee17c168367b3ef3fa2dbef46bc9e
piscue <piscue@gmail.com>
2021-11-15 23:21:18 +0100

    Initial posix approach

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 set -e
+
+config_api () {
 # Point to the internal API server hostname
 APISERVER=https://kubernetes.default.svc
 
@@ -21,11 +23,94 @@ CACERT=${SERVICEACCOUNT}/ca.crt
 
 # Shorten the curl commands
 CURL_COMMAND=$(which curl)
-CACERT_ARG="--cacert ${CACERT}"
 BASE_URL="$APISERVER/api/v1/namespaces/$NAMESPACE"
-REQUEST_ARG="-X GET $BASE_URL"
+
+POD_FILE=/tmp/pods.json
+}
+
+refresh_api () {
+config_api
+$CURL_COMMAND --cacert ${CACERT} \
+	--header "$HEADER" \
+	-q -s \
+	-X GET "$BASE_URL"/pods \
+	> $POD_FILE
+}
+
+if [ -z "$LABELS" ]; then
+		echo "No labels specified"
+		exit 1
+fi
+LABELS_FILE=/tmp/labels.txt
+# shellcheck disable=SC2016
+AWK_SPLIT_LABELS='{ for (i = 1; i <= NF; ++i ) print $i }'
+echo "$LABELS" | awk -F, "$AWK_SPLIT_LABELS" > $LABELS_FILE
+NUMBER_OF_LABELS_TO_MATCH=$(wc -l < "$LABELS_FILE") # TODO don't match
+echo "$NUMBER_OF_LABELS_TO_MATCH labels to match with running pods: "
+cat $LABELS_FILE
+echo ""
+
+discover_pods () {
+# shellcheck disable=SC2016
+jq_program='
+	.items[]
+|		.metadata.name as $name
+|	  .status.phase as $status
+|		.metadata.labels as $labels
+|	  ( $labels
+|		to_entries
+|   map("\(.key)=\(.value)") )
+|   join(",") as $labels_array
+|  	[$name, $labels_array, $status]
+|   @tsv
+'
+
+PODS_DISCOVERED_FILE=/tmp/pods_discovered.txt
+jq -r "$jq_program" < $POD_FILE > $PODS_DISCOVERED_FILE
+
+echo "Running pods: "
+awk '/Running/ {print $1}' < $PODS_DISCOVERED_FILE
+echo ""
+}
 
 
-PODS="$($CURL_COMMAND $CACERT_ARG --header "$HEADER" $REQUEST_ARG/pods)"
+# Loop until all the labels has been match
+if [ -z "$WAIT_TIME" ]; then
+	WAIT_TIME=10
+fi
 
-echo "$PODS"
+MATCH=0
+while true; do
+	refresh_api
+	discover_pods
+	while read -r pod_info; do
+		POD_NAME=$(echo "$pod_info" | awk '{print $1}')
+		POD_LABELS=$(echo "$pod_info" | awk '{print $2}')
+		POD_STATUS=$(echo "$pod_info" | awk '{print $3}')
+		for POD_LABEL in $(echo "$POD_LABELS" | awk -F, "$AWK_SPLIT_LABELS") ; do
+			while read -r LABEL_TO_MATCH; do
+				if [ "$POD_LABEL" = "$LABEL_TO_MATCH" ]; then
+					echo "MATCH: $POD_LABEL"
+					MATCH=$((MATCH+1))
+					if [ "$MATCH" -eq "$NUMBER_OF_LABELS_TO_MATCH" ]; then
+						if [  "$POD_STATUS" = "Running" ]; then
+							echo "Pod $POD_NAME is running"
+							echo "With this labels:"
+							cat "$LABELS_FILE"
+							echo "Pod info"
+							echo "$pod_info"
+							exit 0
+						fi
+						echo "Pod: $POD_NAME is not yet running"
+						echo "the status is $POD_STATUS"
+					fi
+				fi
+			done < $LABELS_FILE
+		done
+	MATCH=0
+	done < $PODS_DISCOVERED_FILE
+	# echo a timestamp
+	echo "Waiting $WAIT_TIME seconds; timestamp: $(date -u +%Y%m%d%H%M%S)"
+	echo ""
+	sleep $WAIT_TIME
+done
